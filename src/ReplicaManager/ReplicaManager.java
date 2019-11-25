5932ed4b24eb35861d9f212aaf1cbe523ee367f9
Kadeem Caines <kadeemcaines@gmail.com>
2019-11-24 23:44:40 -0500

    Update

---
+++ src/ReplicaManager/ReplicaManager.java
@@ -0,0 +1,26 @@
+package ReplicaManager;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+
+import org.omg.PortableServer.POA;
+
+import com.sun.corba.se.internal.iiop.ORB;
+
+public class ReplicaManager {
+	
+	private String RMHost;
+	private static final int FAILURE_COUNT = 3;
+	
+	
+	
+		public void startReplica() {
+			
+			
+		}
+		
+		public void listenFE() {
+			
+			
+		}
+
+}
--- src/Sequencer/Sequencer.java
+++ src/Sequencer/Sequencer.java
@@ -1,5 +1,56 @@
 package Sequencer;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.MulticastSocket;
+
 public class Sequencer {
+	
+	public static int UDPPortSequencer = 2020;
+	
+	public static void main(String args[]){
+		
+		
+	
+		
+		DatagramSocket dataSocket = null;
+		
+	
+		try
+		{
+			
+	dataSocket = new DatagramSocket(UDPPortSequencer);
+			
+	System.out.println(" DataSocket Created.");
+			
+	
+byte[] buffer = new byte[1024];
+			
+System.out.println(" Buffer Array Created.");
+			
+		
+			while(true)
+			{
+		DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+		dataSocket.receive(request);
+		String reply = "reply";
+		buffer = reply.getBytes();
+		DatagramPacket replyPackey = new DatagramPacket(buffer, buffer.length, request.getAddress(), request.getPort());
+		dataSocket.send(replyPackey);
+		System.out.println("UDP Server on port " + UDPPortSequencer + " is now listening.");
+			}
+		}
+		catch (Exception e)
+		{
+			System.err.println(" Unable to start port listening");
+			System.err.println(e.getMessage());
+		}
+		finally
+		{
+			dataSocket.close();	
+			System.out.println(" DataSocket Closed.");
+		}
+	}
+
 
 }
